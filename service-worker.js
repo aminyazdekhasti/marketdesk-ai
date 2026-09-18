9f27a917e55fa69eb1b4e5a39700de882bf053bb
aminyazdekhasti <aminyazdekhasti@gmail.com>
2026-09-18 06:10:02 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "marketdeskai-v1";
-const ASSETS = ["./MarketDeskAI.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(() => {})));
