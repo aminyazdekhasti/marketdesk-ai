27f05058682e961681f7c8352053da7e8e15fc33
aminyazdekhasti <aminyazdekhasti@gmail.com>
2026-09-18 05:03:08 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "marketdeskai-v1";
+const ASSETS = ["./MarketDeskAI.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).then(r => {
+    const cp = r.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(e.request, cp)).catch(() => {});
+    return r;
+  }).catch(() => c)));
+});
